9ed05e6e634e2f6d7daf54c411646de9467f61d7
anujgawde <gawdeanuj@gmail.com>
2025-03-04 02:14:55 -0500

    fixed routing issue from jobs page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,14 +11,19 @@ export default function Home() {
   const [isLoading, setIsLoading] = useState(false);
 
   const submitEmailHandler = async (e: React.FormEvent<HTMLFormElement>) => {
-    setIsLoading(true);
     e.preventDefault();
+    setIsLoading(true);
     const form = e.target as HTMLFormElement;
     const userData = {
       email: form.email.value,
     };
     const verificationResult = await sendVerificationEmail(userData);
+    console.log(verificationResult);
     if (verificationResult.verified) {
+      const loggedInUser = localStorage.getItem("verifiedEmail");
+      if (!loggedInUser) {
+        localStorage.setItem("verifiedEmail", verificationResult.email);
+      }
       router.push("/jobs");
     } else {
       setIsConfirmationSent(true);
@@ -41,9 +46,6 @@ export default function Home() {
               className="p-2 outline-none border rounded-lg w-full"
               placeholder="johndoe@xyz.com"
             />
-            {/* <button className="rounded-lg bg-primary text-white px-4 py-2 font-semibold">
-                Enter
-              </button> */}
             <button
               disabled={isLoading}
               className="rounded-lg bg-primary disabled:opacity-80 disabled:hover:bg-primary text-white px-4 py-2 font-semibold hover:bg-primary/90 focus:outline-none focus:ring-2 focus:ring-primary focus:ring-offset-2 transition-colors"
